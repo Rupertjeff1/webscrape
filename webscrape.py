4600c97c0ff0642343ef2f2cf420000692a9ad69
Jeff Rupert <89801313+Rupertjeff1@users.noreply.github.com>
2025-04-14 20:52:15 -0400

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -5,18 +5,19 @@ import sys
 stdoutOrigin=sys.stdout
 sys.stdout = open('log.txt', "w")
 
-html_text = requests.get('https://www.mayhemathletes.com/daily-workout/')
+html_text = requests.get('https://www.crossfitepicallyawesome.com/wod/')
 soup = BeautifulSoup(html_text.text, 'lxml')
-workouts = soup.find_all('div', class_ = 'tb-field')
 
+workouts = soup.find_all('p')
+
+for br in soup.find_all('br'):
+    br.replace_with('\n')
 
 for workout in workouts:
     print(workout.text)
     
 sys.stdout.close()
 sys.stdout=stdoutOrigin
-    
-    
     
 
 
